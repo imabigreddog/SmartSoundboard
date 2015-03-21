a69d60efa5ef696d47b50b906aca8724606e87c4
Rushad Antia <nope>
2015-03-21 18:11:40 -0400

    fixed major alertdialog bug
    dank

--- src/com/smartutils/smartsoundboard/MainActivity.java
+++ src/com/smartutils/smartsoundboard/MainActivity.java
@@ -3,20 +3,16 @@ package com.smartutils.smartsoundboard;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.AlertDialog.Builder;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.widget.EditText;
-import android.widget.FrameLayout;
 
 public class MainActivity extends Activity implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 	
 	private NavigationDrawerFragment mNavigationDrawerFragment;
-	private EditText input;
 	private CharSequence mTitle;
-	private FrameLayout background;
-	private Builder dialog;
+	private boolean run = false;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,27 +22,6 @@ public class MainActivity extends Activity implements NavigationDrawerFragment.N
 		
 		mNavigationDrawerFragment = (NavigationDrawerFragment) getFragmentManager().findFragmentById(R.id.navigation_drawer);
 		mTitle = getTitle();
-		input = new EditText(MainActivity.this);
-		background = (FrameLayout) findViewById(R.id.background);
-		
-		dialog = new AlertDialog.Builder(MainActivity.this).setTitle("Please Enter a Link to a sound file").setView(input)
-				.setPositiveButton("Download", new DialogInterface.OnClickListener() {
-					
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-					
-						// TODO download form link and launch progress dialog.
-						
-					}
-				}).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-					
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-					
-						dialog.dismiss();
-						
-					}
-				}).setCancelable(false);
 		
 		// Set up the drawer.
 		mNavigationDrawerFragment.setUp(R.id.navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout));
@@ -55,9 +30,34 @@ public class MainActivity extends Activity implements NavigationDrawerFragment.N
 	public final void onNavigationDrawerItemSelected(int position) {
 	
 		System.out.println(position);
-		if (position == 0) {
-			// dialog.show();
-		}
+		
+		// run is for weird android thing
+		if (position == 0 && run) {
+			new AlertDialog.Builder(MainActivity.this).setTitle("Please Enter a Link to a sound file").setView(new EditText(MainActivity.this))
+					.setPositiveButton("Download", new DialogInterface.OnClickListener() {
+						
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+						
+							// TODO download form link and launch progress dialog.
+							dialog.cancel();
+							dialog.dismiss();
+							dialog = null;
+						}
+					}).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+						
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+						
+							dialog.cancel();
+							dialog.dismiss();
+							dialog = null;
+							System.gc();
+						}
+					}).setCancelable(false).show();
+			
+		} else
+			run = true;
 	}
 	
 	public void onSectionAttached(int number) {
