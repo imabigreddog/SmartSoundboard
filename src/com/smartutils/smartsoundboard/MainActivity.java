7650433158854faa89baec28f2b43af8b29cef9f
Rushad Antia <nope>
2015-03-21 17:42:59 -0400

    added progress diarog

--- src/com/smartutils/smartsoundboard/MainActivity.java
+++ src/com/smartutils/smartsoundboard/MainActivity.java
@@ -2,14 +2,21 @@ package com.smartutils.smartsoundboard;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.widget.EditText;
+import android.widget.FrameLayout;
 
 public class MainActivity extends Activity implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 	
 	private NavigationDrawerFragment mNavigationDrawerFragment;
-	
+	private EditText input;
 	private CharSequence mTitle;
+	private FrameLayout background;
+	private Builder dialog;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,21 +26,44 @@ public class MainActivity extends Activity implements NavigationDrawerFragment.N
 		
 		mNavigationDrawerFragment = (NavigationDrawerFragment) getFragmentManager().findFragmentById(R.id.navigation_drawer);
 		mTitle = getTitle();
+		input = new EditText(MainActivity.this);
+		background = (FrameLayout) findViewById(R.id.background);
+		
+		dialog = new AlertDialog.Builder(MainActivity.this).setTitle("Please Enter a Link to a sound file").setView(input)
+				.setPositiveButton("Download", new DialogInterface.OnClickListener() {
+					
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+					
+						// TODO download form link and launch progress dialog.
+						
+					}
+				}).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+					
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+					
+						dialog.dismiss();
+						
+					}
+				}).setCancelable(false);
 		
 		// Set up the drawer.
 		mNavigationDrawerFragment.setUp(R.id.navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout));
 	}
 	
-	@Override
-	public void onNavigationDrawerItemSelected(int position) {
+	public final void onNavigationDrawerItemSelected(int position) {
 	
 		System.out.println(position);
+		if (position == 0) {
+			// dialog.show();
+		}
 	}
 	
 	public void onSectionAttached(int number) {
 	
 		switch (number) {
-		case 1:
+		case 0:
 			mTitle = getString(R.string.download);
 			break;
 		}
--- src/com/smartutils/smartsoundboard/NavigationDrawerFragment.java
+++ src/com/smartutils/smartsoundboard/NavigationDrawerFragment.java
@@ -4,22 +4,17 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.SharedPreferences;
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 /**
  * Fragment used for managing interactions for and presentation of a navigation drawer.
@@ -170,53 +165,6 @@ public class NavigationDrawerFragment extends Fragment {
 	
 		super.onSaveInstanceState(outState);
 		outState.putInt(STATE_SELECTED_POSITION, mCurrentSelectedPosition);
-	}
-	
-	@Override
-	public void onConfigurationChanged(Configuration newConfig) {
-	
-		super.onConfigurationChanged(newConfig);
-		// Forward the new configuration the drawer toggle component.
-		mDrawerToggle.onConfigurationChanged(newConfig);
-	}
-	
-	@Override
-	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-	
-		// If the drawer is open, show the global app actions in the action bar. See also
-		// showGlobalContextActionBar, which controls the top-left area of the action bar.
-		if (mDrawerLayout != null && isDrawerOpen()) {
-			inflater.inflate(R.menu.global, menu);
-			showGlobalContextActionBar();
-		}
-		super.onCreateOptionsMenu(menu, inflater);
-	}
-	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-	
-		if (mDrawerToggle.onOptionsItemSelected(item)) {
-			return true;
-		}
-		
-		if (item.getItemId() == R.id.action_example) {
-			Toast.makeText(getActivity(), "Example action.", Toast.LENGTH_SHORT).show();
-			return true;
-		}
-		
-		return super.onOptionsItemSelected(item);
-	}
-	
-	/**
-	 * Per the navigation drawer design guidelines, updates the action bar to show the global app
-	 * 'context', rather than just what's in the current screen.
-	 */
-	private void showGlobalContextActionBar() {
-	
-		ActionBar actionBar = getActionBar();
-		actionBar.setDisplayShowTitleEnabled(true);
-		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-		actionBar.setTitle(R.string.app_name);
 	}
 	
 	private ActionBar getActionBar() {
