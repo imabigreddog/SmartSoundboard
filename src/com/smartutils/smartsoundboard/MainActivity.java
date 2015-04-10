d6c3bd2a24ccff5e2c3855d24997facc39c95853
stallmaninterjector <chooba1>
2015-04-09 22:28:44 -0400

    remove player once it's killed

--- src/com/smartutils/smartsoundboard/MainActivity.java
+++ src/com/smartutils/smartsoundboard/MainActivity.java
@@ -118,6 +118,7 @@ public class MainActivity extends Activity implements NavigationDrawerFragment.N
 						mp.stop();
 						mp.reset();
 						mp.release();
+						players.remove(mp);
 					} catch (Exception e) {
 						e.printStackTrace();
 					}
